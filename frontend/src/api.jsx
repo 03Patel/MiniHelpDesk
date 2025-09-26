8c042dda3578ff772862ca974466ee20db928d09
Ganesh Ji Patel <161694533+03Patel@users.noreply.github.com>
2025-09-26 13:53:33 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/", // backend URL
+  baseURL: "https://minibackend-t51q.onrender.com/api/", // backend URL
 });
 
 export default API;
